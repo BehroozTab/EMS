170ab75669fcb200e14950d1d7763d19ff2d6b19
Behrooz Tabrizian <t_behrooz2000@yahoo.com>
2019-04-02 16:45:27 -0400

    Add files via upload

---
+++ Login/Includes/library.php
@@ -0,0 +1,10 @@
+<?php
+$dbhost="localhost";
+$dbuser="root";
+$dbpassword= "";
+$dbdatabasename="EmpDataBase";
+$conn=mysql_connect($dbhost,$dbuser,$dbpassword);
+mysql_select_db($dbdatabasename,$conn);
+
+?>
+
